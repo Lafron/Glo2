893047606e8a09d6f62af67c87159f23f2c975c4
Epshtein Dmitry <letuver@yahoo.com>
2023-07-18 00:57:08 +0300

    add master to the div

--- scripts.js
+++ scripts.js
@@ -3,6 +3,12 @@ const b = "new variens";
 const a = "first variens";
 const c = "second variens";
 
+const m = document.getElementById("master");
+
 function l() {
     console.log("All command for git");
-}
+};
+
+const f = () => {
+    m.textContent = "master";
+ };
